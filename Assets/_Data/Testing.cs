6eef7446494331ce697574a5538c2c942b78c4d4
phucnds <42528099+phucnds@users.noreply.github.com>
2023-03-01 08:35:24 +0700

    - update

---
+++ Assets/_Data/Testing.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    
+}
